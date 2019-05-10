b0bee7b72923413bc2e4475b50d5ed6a009f9358
Waiel Eid <waiel@waieleid.com>
2019-05-10 13:48:59 +0300

    inital code and views

---
+++ OnTheMap/LoginViewController.swift
@@ -0,0 +1,20 @@
+//
+//  LoginViewController.swift
+//  OnTheMap
+//
+//  Created by Waiel Eid on 10/5/19.
+//  Copyright © 2019 Waiel Eid. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
